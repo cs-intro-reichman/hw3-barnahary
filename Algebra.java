c94bcfa02de9245758e46b9ac8c197c2f59de808
barnahary <bar.nahary@post.runi.ac.il>
2024-11-28 10:28:55 +0200

    done

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,74 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x1; i++) {
+			x2++;
+		}
+		
+		return x2;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i =0; i < x2; i++) {
+			x1--;
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int outcome = 0;
+		for (int i = 0; i < x2; i++) {
+		outcome = plus(outcome, x1);
+		}
+		return outcome;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		for (int i = 0; i < n; i++) {
+			result = times(result, x);
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int times = 0;
+		while (x1 >= x2) {
+			x1 = minus(x1, x2);
+			times++;
+		}
+		return times;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int firstStep = div(x1, x2);
+		int secondStep = times(firstStep, x2);
+		int thirdStep = minus(x1, secondStep );
+		return thirdStep;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+		if (x == 0) {
+			return 0;
+		}
+		int finel = div(x, 2);
+		int prev = 0;
+		
+		while (finel != prev) {
+			prev = finel;
+
+			int firstStep = div(x, finel);
+			int secondStep = plus(finel, firstStep);
+			finel = div(secondStep, 2); 
+		}
+		
+		return finel;	 	
+		
+	}
 }
--- Anagram.java
+++ Anagram.java
@@ -5,10 +5,15 @@ public class Anagram {
 		System.out.println(isAnagram("silent","listen"));  // true
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort"));// true
+		System.out.println(isAnagram("Tom Marvolo Ridle","I am Lord Voldemort")); // false
+		System.out.println(isAnagram("ababa","aabb")); // false
+		
 
 		// Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
+		System.out.println(preProcess("Tom Marvolo Riddle"));
+		System.out.println(preProcess("Bar Nahary ? Richman"));
 		
 		// Tests the randomAnagram function.
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
@@ -28,22 +33,65 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
+		String sTr1 = preProcess(str1);
+		String sTr2 = preProcess(str2);
+		if (sTr1.length() != sTr2.length()) {
+			return false;
+		} // if the Strings are not equal - definetly not an anagram.
+		  // first loop : 
+			for (int i = 0; i < sTr1.length(); i++) {
+				char c = sTr1.charAt(i);
+				// another loop ! 
+				boolean sec = false;
+				for (int j = 0; j < sTr2.length(); j++) {
+					if (sTr2.charAt(j) == c) {
+						sec = true;
+						break;
+					}
+				}
+				if (!sec) {
+					return false;
+
+			}
+			}
+			return true; 
+		}
+
+	
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
-	} 
+		// first loop - for the Upper-lower cases : 
+		String outcome = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+		if (Character.isUpperCase(c)) {
+				outcome += Character.toLowerCase(c);
+			// second condition
+			} else if (c >= 'a' && c <= 'z' ) {
+				outcome += c;
+			}  
+		}
+			return outcome;
+			}
+			
+
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String sTr = preProcess(str);
+		String outcome = "";
+		// a while loop : 
+		while (sTr.length() > 0) {
+			int randomeChar = (int) (Math.random() * sTr.length());
+			outcome += sTr.charAt(randomeChar);
+			sTr = sTr.substring(0, randomeChar) + sTr.substring(randomeChar + 1);
+		}
+
+		return outcome;
 	}
 }
